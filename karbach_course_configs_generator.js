83cf227f6781e094d2f634018573c6f11e2fa83b
francesko <francesco.macri1986@gmail.com>
2017-03-21 18:50:41 +0100

    generate static screen configs

--- karbach_course_configs_generator.js
+++ karbach_course_configs_generator.js
@@ -5,20 +5,24 @@ var RowList = require('./app/RowList');
 var GoogleAPI = require('./app/GoogleAPI');
 var configPath = './output/config.json';
 
+String.prototype.lowercaseFirstLetter = function() {
+    return this.charAt(0).toLowerCase() + this.slice(1);
+};
+
 var tabs = {
   'motivational_1': 0,
   'motivational_2': 1,
   'motivational_3': 2,
   'unmotivational_1': 3,
   'unmotivational_2': 4,
-  'unmotivational_3': 5,
+  'unmotivational_3': 5
 };
  
 var rowList = new RowList();
 
 GoogleAPI.getScreeFlowData(tabs['motivational_1'])
 .then(function(rows){
-  generateConfig(rows, 21);
+  generateConfig(rows, 1);
 })
 .catch(function(err){
   console.log('error', err);
@@ -27,46 +31,19 @@ GoogleAPI.getScreeFlowData(tabs['motivational_1'])
   console.log('done');
 });
 
-function generateSession(config, rowList, session) {
-  var rows = rowList.getRowsBySession(session);
-  var imageBasePath = "https:\/\/contentimg.s3.amazonaws.com/karbach/mainstudy/";
-
-  for(var i = 0; i < rows.length; i = i + 1) {
-    let row = rows[i];
-
-    switch(row.type) {
-      case 'Welcome':
-      case 'Text':  {
-        if (i == 0) {
-          config.staticScreen[session - 1] = [
-            {
-              "image": imageBasePath + row.image,
-              "text": row.mainText.replace(/\n/g, '<br>').replace('\n\nWenn du die ganze Geschichte noch einmal hören möchtest, klicke auf „nochmal anhören“\nWenn du mit dem Spiel weitermachen möchtest klicke auf „weiter“\n', ''),
-              "button": 'Weiter'
-            },
-            [],
-            [],
-            []
-          ];
-        }
-      } break;
-
-      case 'Video': {
-
-      } break;
-
-      case 'Points': {
-
-      } break;
-    }
-  }
-}
-
 function generateConfig(rows, totalSessions) {
+  rows.forEach(function(rowData) {
+    var row = new Row(rowData);
+    
+    if (row.type !== 'Start Course') {
+      rowList.add(row);
+    }
+  });
+
   var config = {
-    games_map_names: [],
-    games_map: [],
-    games_plan: [],
+    // games_map_names: [],
+    // games_map: [],
+    // games_plan: [],
     introVideos: [],
     tutorialVideos: [],
     staticScreen: [],
@@ -78,14 +55,6 @@ function generateConfig(rows, totalSessions) {
     postEsScreen: []
   };
 
-  rows.forEach(function(rowData) {
-    var row = new Row(rowData);
-    
-    if (row.type !== 'Start Course') {
-      rowList.add(row);
-    }
-  });
-
   for(var i = 1; i <= totalSessions; i = i + 1) {
     generateSession(config, rowList, i)
   }
@@ -93,4 +62,89 @@ function generateConfig(rows, totalSessions) {
   fs.writeFile(configPath, JSON.stringify(config, null, 4), function (err) {
     if (err) return console.log(err);
   });
+}
+
+function generateSession(config, rowList, session) {
+  var screenFlow = [
+    'StaticScreen',
+    'IntroVideo',
+    'PreES',
+    'PseudoChoice',
+    'PreESVideo',
+    'game',
+    'StaticScreen2',
+    'PostES',
+    'PostESVideo',
+    'StaticScreen3'
+  ];
+
+  var rows = rowList.getRowsBySession(session);
+  var imageBasePath = 'https:\/\/contentimg.s3.amazonaws.com/karbach/mainstudy/';
+  var previousType = null;
+  var sessionIndex = session - 1;
+  var exerciseIndex = 0;
+
+  config.introVideos[sessionIndex] = [];
+  config.tutorialVideos[sessionIndex] = [];
+  config.staticScreen[sessionIndex] = [];
+  config.staticScreen2[sessionIndex] = [];
+  config.staticScreen3[sessionIndex] = [];
+  config.staticScreen4[sessionIndex] = [];
+  config.pseudoChoice[sessionIndex] = [];
+  config.preEsScreen[sessionIndex] = [];
+  config.postEsScreen[sessionIndex] = [];
+
+  for(var i = 0; i < rows.length; i = i + 1) {
+    let row = rows[i];
+
+    switch(row.type) {
+      case 'StaticScreen':
+      case 'StaticScreen2':
+      case 'StaticScreen3':
+      case 'StaticScreen4': {
+        config[row.type.lowercaseFirstLetter()][sessionIndex][exerciseIndex] = {
+          'image': imageBasePath + row.image,
+          'text': row.mainText.replace(/\n/g, '<br>').replace('\n\nWenn du die ganze Geschichte noch einmal hören möchtest, klicke auf „nochmal anhören“\nWenn du mit dem Spiel weitermachen möchtest klicke auf „weiter“\n', ''),
+          'button': 'Weiter'
+        };
+      } break;
+    }
+
+    if (previousType != null && screenFlow.indexOf(row.type) < screenFlow.indexOf(previousType)) {
+      console.log(row.type, previousType, exerciseIndex);
+
+      if (!config.introVideos[sessionIndex][exerciseIndex]) {
+        config.introVideos[sessionIndex][exerciseIndex] = [];
+      }
+      if (!config.tutorialVideos[sessionIndex][exerciseIndex]) {
+        config.tutorialVideos[sessionIndex][exerciseIndex] = [];
+      }
+      if (!config.staticScreen[sessionIndex][exerciseIndex]) {
+        config.staticScreen[sessionIndex][exerciseIndex] = [];
+      }
+      if (!config.staticScreen2[sessionIndex][exerciseIndex]) {
+        config.staticScreen2[sessionIndex][exerciseIndex] = [];
+      }
+      if (!config.staticScreen3[sessionIndex][exerciseIndex]) {
+        config.staticScreen3[sessionIndex][exerciseIndex] = [];
+      }
+      if (!config.staticScreen4[sessionIndex][exerciseIndex]) {
+        config.staticScreen4[sessionIndex][exerciseIndex] = [];
+      }
+      if (!config.pseudoChoice[sessionIndex][exerciseIndex]) {
+        config.pseudoChoice[sessionIndex][exerciseIndex] = [];
+      }
+      if (!config.preEsScreen[sessionIndex][exerciseIndex]) {
+        config.preEsScreen[sessionIndex][exerciseIndex] = 0;
+      }
+      if (!config.postEsScreen[sessionIndex][exerciseIndex]) {
+        config.postEsScreen[sessionIndex][exerciseIndex] = 0;
+      }
+
+      exerciseIndex = exerciseIndex + 1;
+      previousType = null;
+    } else {
+      previousType = row.type;
+    }
+  }
 }
