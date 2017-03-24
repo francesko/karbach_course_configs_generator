700499656f6e7a64de8c171ba232e735bb8ea2c4
francesko <francesco.macri1986@gmail.com>
2017-03-24 16:43:45 +0100

    generate games groups config

--- app/generateConfig.js
+++ app/generateConfig.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
+var utils = require('./utils');
+var generateSession = require('./generateSession');
 var Row = require('./Row');
 var RowList = require('./RowList');
-var generateSession = require('./generateSession');
 
 module.exports = function(rows, totalSessions, outputPath) {
   var rowList = new RowList();
@@ -17,6 +18,7 @@ module.exports = function(rows, totalSessions, outputPath) {
   var config = {
     games_map_names: [],
     games_map: [],
+    games_groups: [],
     games_plan: [],
     introVideos: [],
     tutorialVideos: [],
@@ -32,16 +34,11 @@ module.exports = function(rows, totalSessions, outputPath) {
     generateSession(config, rowList, sessionIndex);
   }
 
-  var gameIndex1 = 0;
-  var gameIndex2 = 1;
-
   for(let sessionIndex = 1; sessionIndex <= totalSessions; sessionIndex = sessionIndex + 1) {
-    config.games_plan[sessionIndex - 1] = Array.apply(null, new Array(config.games_map.length)).map(Number.prototype.valueOf, 0);
-    config.games_plan[sessionIndex - 1][gameIndex1] = 2;
-    config.games_plan[sessionIndex - 1][gameIndex2] = 2;
-    gameIndex1 = gameIndex1 + 2;
-    gameIndex2 = gameIndex2 + 2;
+    config.games_plan.push(utils.getGamesPlan(config.games_map, sessionIndex));
   }
+
+  config.games_groups = utils.getGamesGroups(config.games_map);
 
   fs.writeFile(outputPath, JSON.stringify(config, null, 2).replace(/https:\/\//g, 'https:\\/\\/'), function (err) {
     if (err) return console.log(err);
--- app/utils.js
+++ app/utils.js
@@ -36,6 +36,42 @@ module.exports = {
 
       return memo;
     }, {});
+  },
+
+  getGamesPlan: function(gamesMap, sessionIndex) {
+    var sessionGamesPlan = Array.apply(null, new Array(gamesMap.length)).map(Number.prototype.valueOf, 0);
+
+    sessionIndex = sessionIndex - 1;
+
+    var gameIndex1 = (sessionIndex === 0) ? 0 : (sessionIndex * 2);
+    var gameIndex2 = (sessionIndex === 0) ? 1 : (sessionIndex * 2 + 1);
+
+    sessionGamesPlan[gameIndex1] = 2;
+    sessionGamesPlan[gameIndex2] = 2;
+
+    return sessionGamesPlan;
+  },
+
+  getGamesGroups: function(gamesMap) {
+    var gamesGroups = gamesMap.reduce(function(memo, conceptId) {
+      var groupName = conceptId.replace(/[0-9]?_ID/, '');
+
+      if (!memo[groupName]) {
+        memo[groupName] = [];
+      }
+
+      if (memo[groupName].indexOf(conceptId) === -1) {
+        memo[groupName].push(conceptId);
+      }
+
+      memo[groupName].sort();
+
+      return memo;
+    }, {});
+
+    return Object.keys(gamesGroups).map(function (key) { 
+      return gamesGroups[key]; 
+    });
   }
 
 };
